05eff749dc0f162575a06df3821eab4f5855e3c8
Milivoje Popovac <popovacmilivoje993@gmail.com>
2023-01-30 21:36:48 +0100

    Delete Mainnet from note

--- src/App.js
+++ src/App.js
@@ -392,7 +392,7 @@ function App() {
             }}
           >
             Please make sure you are connected to the right network (
-            {CONFIG.NETWORK.NAME} Mainnet) and the correct address. Please note:
+            {CONFIG.NETWORK.NAME}) and the correct address. Please note:
             Once you make the purchase, you cannot undo this action.
           </s.TextDescription>
           <s.SpacerSmall />
